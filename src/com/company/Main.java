2b57b3c51ea63cc51a58adca1e3b20ca46c4b313
Andre Lussier <Dremon7@hotmail.com>
2019-10-01 22:20:48 -0400

    zlib deflator

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,15 @@
 package com.company;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
+import org.omg.IOP.Encoding;
+import sun.nio.cs.ext.DoubleByte;
+
+import java.io.*;
+import java.util.Base64;
 import java.util.zip.Deflater;
 import java.util.zip.DeflaterOutputStream;
 import java.util.zip.InflaterInputStream;
+
+import static com.sun.xml.internal.messaging.saaj.packaging.mime.util.ASCIIUtility.getBytes;
 
 public class Main {
 
@@ -32,6 +35,50 @@ public class Main {
         in.close();
         bout.close();
         return bout.toByteArray();
+    }
+
+    private String zipCompression(String data) throws UnsupportedEncodingException, IOException{
+        Deflater zipDeflater = new Deflater();
+        ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+        try{
+            zipDeflater.setInput(getBytes(data)); // this might be wrong one
+            zipDeflater.finish();
+            byte[] buffer = new byte[1024];
+            int count = 0;
+
+            while(!zipDeflater.finished()){
+                count = zipDeflater.deflate(buffer);
+                stream.write(buffer, 0,count);
+            }
+            // this needs to be fixed
+            return new String(Base64.getEncoder().encode(stream.toByteArray()), LOCAL_ENCODING);
+        }finally {
+            stream.close();
+            zipDeflater.end();
+        }
+    }
+
+    public static byte[] compressForzLIB(byte[] bytesToCompress){
+        Deflater deflater = new Deflater();
+        deflater.setInput(bytesToCompress);
+        deflater.finish();
+
+        byte[] bytesToCompressed = new byte[Short.MAX_VALUE];
+
+        int numberOfBytesAfterCompression = deflater.deflate(bytesToCompress);
+
+        byte[] returnValues = new byte[numberOfBytesAfterCompression];
+
+        System.arraycopy(
+                bytesToCompressed,
+                0,
+                returnValues,
+                0,
+                numberOfBytesAfterCompression
+        );
+
+        return returnValues;
     }
 
 
